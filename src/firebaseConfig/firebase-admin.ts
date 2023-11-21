93d2ecaca51fc7b494652c7fae29da6d37e42c78
weedeej <devildjoseph666@gmail.com>
2023-11-21 10:26:25 +0800

    upated firebase-admin config

--- src/firebaseConfig/firebase-admin.ts
+++ src/firebaseConfig/firebase-admin.ts
@@ -1,9 +1,13 @@
 import admin from "firebase-admin";
 
-const serviceAccountConfig = {
-  
+const serviceAccountConfig:admin.ServiceAccount = {
+  projectId: process.env["FIREBASE_SA_PROJECTID"],
+  privateKey: process.env["FIREBASE_SA_PRIVATE_KEY"],
+  clientEmail: process.env["FIREBASE_SA_CLIENT_EMAIL"],
 }
 
 const app = admin.apps.length < 1 ? admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountConfig)
+  credential: admin.credential.cert(serviceAccountConfig),
 }) : admin.app();
+export const adminDb = admin.firestore(app);
+export const adminAuth = admin.auth(app);
